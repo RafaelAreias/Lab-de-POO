1ed9d7fd95f7b271e5dbaa0c2269a40212a40f75
RafaelAreias <a.silveira.rafael@gmail.com>
2024-10-24 18:20:26 -0300

    Add files via upload

---
+++ Lab_POO_9/src/Inventario.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Inventario {
+
+    ArrayList<Livro> livros = new ArrayList<>();
+
+    public void adicionarLivro(Livro livro) {
+
+    }
+
+    public void exibirLivros(){
+
+    }
+}
---
+++ Lab_POO_9/src/Livro.java
@@ -0,0 +1,10 @@
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+
+    }
+}
