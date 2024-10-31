980701a10cbaf132df66c49b6bc15e5c9955cb3d
RafaelAreias <a.silveira.rafael@gmail.com>
2024-10-31 19:45:27 -0300

    Add files via upload

---
+++ Lab_POO_10/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Arquivo arquivo = new Arquivo();
+
+        Jogo jogo1 = new Jogo(1,"GTA",105.00);
+        Jogo jogo2 = new Jogo(2,"Far Cry",149.99);
+
+        arquivo.escrever(jogo1);
+        arquivo.escrever(jogo2);
+
+        arquivo.lerJogo(149.99);
+        arquivo.lerJogos();
+    }
+}
