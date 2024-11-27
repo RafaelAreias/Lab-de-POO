6432136d56ab3817d8fd4ce0e1223c94e0a7c227
RafaelAreias <a.silveira.rafael@gmail.com>
2024-11-26 21:41:11 -0300

    Delete Trabalho_POO_BD directory

--- Trabalho_POO_BD/src/main/java/br/inatel/DAO/OperacoesCRUD.java
+++
@@ -1,9 +0,0 @@
-package br.inatel.DAO;
-import java.util.ArrayList;
-
-public interface OperacoesCRUD<Tipo> {
-    boolean insert(Tipo obj);
-    boolean update(int id, Tipo obj);
-    boolean delete(int id);
-    ArrayList<Tipo> select();
-}
